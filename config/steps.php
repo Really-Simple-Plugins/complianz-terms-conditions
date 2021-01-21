c4e431d234d2678ed69b8ca8715d746170cb7971
AertHulsebos <39100908+AertHulsebos@users.noreply.github.com>
2021-01-21 15:30:44 +0100

    commit/4

--- config/steps.php
+++ config/steps.php
@@ -8,8 +8,8 @@ $this->steps = apply_filters('cmplz_tc_steps',array(
 				"id"    => "company",
 				"title" => __( "General", 'complianz-terms-conditions' ),
 						'intro' => '<h1>'._x('Terms & Conditions','intro first step', 'complianz-terms-conditions').'</h1><p>'.
-						           sprintf(_x('We have tried to make our Wizard as simple and fast as possible. Although these questions are all necessary, if there’s any way you think we can improve the plugin, please let us %sknow%s!','intro first step', 'complianz-terms-conditions'),'<a target="_blank" href="https://complianz.io/contact">', '</a>').'<br>'.
-						           sprintf(_x('Please note that you can always save and finish the wizard later (if you need a break), use our %sdocumentation%s for additional information or log a %ssupport ticket%s if you need our assistance.', 'intro first step', 'complianz-terms-conditions'),'<a target="_blank" href="https://complianz.io/documentation">', '</a>','<a target="_blank" href="https://complianz.io/support">', '</a>').'</p>',
+						           sprintf(_x('We have tried to make our Wizard as simple and fast as possible. Although these questions are all necessary, if there’s any way you think we can improve the plugin, please let us %sknow%s!','intro first step', 'complianz-terms-conditions'),'<a target="_blank" href="https://complianz.io/contact">', '</a>').
+						           sprintf(_x(' Please note that you can always save and finish the wizard later (if you need a break), use our %sdocumentation%s for additional information or log a %ssupport ticket%s if you need our assistance.', 'intro first step', 'complianz-terms-conditions'),'<a target="_blank" href="https://complianz.io/documentation">', '</a>','<a target="_blank" href="https://complianz.io/support">', '</a>').'</p>',
 			),
 
 			2 => array(
@@ -18,28 +18,32 @@ $this->steps = apply_filters('cmplz_tc_steps',array(
 				'sections' => array(
 					1 => array(
 						'title' => __( 'Content', 'complianz-terms-conditions' ),
-						'intro' =>
-                            '<p>'._x( 'Complianz will scan several pages of your website for first-party cookies and known third-party scripts. The scan will be recurring monthly to keep you up-to-date!',
-								'intro scan', 'complianz-terms-conditions' ). '</p>'
-                            .'<p>'._x( 'For more information, ', 'complianz-terms-conditions').'<a href="TODO">'._x('read our 5 tips about the cookie scan.', 'complianz-terms-conditions').'</a></p>',
-					),
+						'intro' => _x( 'These questions will concern the content presented on your website and specific functionalities that might need to be included in the Terms & Conditions.',
+								'intro content', 'complianz-terms-conditions' ). cmplz_tc_read_more( 'https://complianz.io/terms-conditions/' ),
+			),
 					2 => array(
 						'title' => __( 'Communication', 'complianz-terms-conditions' ),
-					),
+						'intro' => _x( 'These questions will explicitly explain your efforts in communicating with your clients and users.', 'intro communication', 'complianz-terms-conditions'),
+
+			),
+
 					3 => array(
-						'title' => __( 'Liability',
-							'complianz-terms-conditions' ),
-					),
+						'title' => __( 'Liability', 'complianz-terms-conditions' ),
+						'intro' => _x( 'liability',
+								'intro liabilty', 'complianz-terms-conditions' ). cmplz_tc_read_more( 'https://complianz.io/terms-conditions/' ),
+
+			),
+
 					4 => array(
 						'title' => __( 'Copyright', 'complianz-terms-conditions' ),
-						'intro' => _x('You can add scripts that should be activated whenever someone accepts the cookie policy. In the third party iframes and scripts sections, you can add URLs from third party scripts that should be blocked until the cookie warning is accepted.', 'intro cookie usage', 'complianz-terms-conditions'),
+						'intro' => _x( 'About Creative Commons etc.',
+								'intro copyright', 'complianz-terms-conditions' ). cmplz_tc_read_more( 'https://complianz.io/terms-conditions/' ),
 					),
 
 					5 => array(
 						'title' => __( 'Returns', 'complianz-terms-conditions' ),
-						'intro' => '<p>'.sprintf(_x( 'Complianz provides your Cookie Policy with comprehensive cookie descriptions, supplied by cookie database.org. We connect to this open-source database using an external API, which sends the results of the cookiescan (a list of found cookies, used plugins and your domain) to cookie database.org, for the sole purpose of providing you with accurate descriptions and keeping them up-to-date at a weekly schedule. For more information, please read %sthis article%s.',
-                                    'complianz-terms-conditions' ).'</p>',
-                                    '<a href="https://complianz.io/our-cookiedatabase-a-new-initiative/">', '</a>' )
+						'intro' => _x( 'About Creative Commons etc.',
+								'intro copyright', 'complianz-terms-conditions' ). cmplz_tc_read_more( 'https://complianz.io/terms-conditions/' ),
 					),
 				),
 			),
