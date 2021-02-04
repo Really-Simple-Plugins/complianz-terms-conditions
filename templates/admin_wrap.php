6880ff35cee9372117df94f5886f1e3b5b917043
AertHulsebos <39100908+AertHulsebos@users.noreply.github.com>
2021-02-04 13:42:47 +0100

    CSS

--- templates/admin_wrap.php
+++ templates/admin_wrap.php
@@ -3,7 +3,7 @@
     <h1 class="cmplz-notice-hook-element"></h1>
 	<div id="cmplz-{page}">
 		<div id="cmplz-header">
-			<img src="<?php echo trailingslashit(cmplz_tc_url)?>assets/images/cmplz-logo.svg">
+			<img src="<?php echo trailingslashit(cmplz_tc_url)?>assets/images/cmplz-logo.svg" alt="Complianz - Terms & conditions">
             <div class="cmplz-header-right">
                 <a href="https://complianz.io/docs/" class="link-black" target="_blank"><?php _e("Documentation", 'complianz-terms-conditions')?></a>
                 <a href="https://complianz.io/support" class="button button-black" target="_blank"><?php echo _e("Support", 'complianz-terms-conditions') ?></a>
