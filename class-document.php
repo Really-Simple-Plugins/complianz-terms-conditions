e6f12cbda265d0742bb3992238b0167fe838cd33
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-02-16 16:13:43 +0100

    obfuscate email

--- class-document.php
+++ class-document.php
@@ -996,8 +996,7 @@ if ( ! class_exists( "cmplz_tc_document" ) ) {
 
 		public function enqueue_assets() {
 			if ( defined('cmplz_version') && $this->is_complianz_page() ) {
-				$min      = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? ''
-					: '.min';
+				$min      = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
 				$load_css = cmplz_get_value( 'use_document_css' );
 				if ( $load_css ) {
 					wp_register_style( 'cmplz-document',
@@ -1005,10 +1004,12 @@ if ( ! class_exists( "cmplz_tc_document" ) ) {
 						cmplz_version );
 					wp_enqueue_style( 'cmplz-document' );
 				}
-
 				add_action( 'wp_head', array( COMPLIANZ::$document, 'inline_styles' ), 100 );
 			}
 
+			if ( !defined('cmplz_version') && $this->is_complianz_page() ) {
+				add_action( 'wp_head', array( $this, 'obfuscate_email_css') );
+			}
 		}
 
 		/**
@@ -1102,6 +1103,12 @@ if ( ! class_exists( "cmplz_tc_document" ) ) {
 
 			return $classes;
 		}
+		public function obfuscate_email_css(){
+			?><style>.cmplz-obfuscate {
+                    direction: rtl;
+                    unicode-bidi: bidi-override;
+                }</style><?php
+		}
 
 		/**
 		 * obfuscate the email address
@@ -1113,15 +1120,12 @@ if ( ! class_exists( "cmplz_tc_document" ) ) {
 
 		public function obfuscate_email( $email ) {
 			$alwaysEncode = array( '.', ':', '@' );
-
 			$result = '';
-
+			$email = strrev($email);
 			// Encode string using oct and hex character codes
 			for ( $i = 0; $i < strlen( $email ); $i ++ ) {
 				// Encode 25% of characters including several that always should be encoded
-				if ( in_array( $email[ $i ], $alwaysEncode )
-				     || mt_rand( 1, 100 ) < 25
-				) {
+				if ( in_array( $email[ $i ], $alwaysEncode ) || mt_rand( 1, 100 ) < 25 ) {
 					if ( mt_rand( 0, 1 ) ) {
 						$result .= '&#' . ord( $email[ $i ] ) . ';';
 					} else {
@@ -1131,12 +1135,7 @@ if ( ! class_exists( "cmplz_tc_document" ) ) {
 					$result .= $email[ $i ];
 				}
 			}
-
-			//make clickable
-
-			$result = '<a href="mailto:'.$result.'">'.$result.'</a>';
-
-			return $result;
+			return '<span class="cmplz-obfuscate" >'.$result.'</span>';
 		}
 
 
