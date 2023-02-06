ddd2da91d0dfa83096cc67826d46175282c6d802
AertHulsebos <39100908+AertHulsebos@users.noreply.github.com>
2023-02-06 13:49:47 +0100

    Update terms-conditions.php

--- config/documents/terms-conditions.php
+++ config/documents/terms-conditions.php
@@ -402,7 +402,7 @@ $this->pages['all']['terms-conditions']['document_elements'] = array(
 	),
 
 	array(
-		'content'   => sprintf(_x( 'If you are 17 years old your Parent or legal guardian must review and agree to these Terms before you use our website any further, and your Parent or legal guardian will be responsible and liable for all of your acts and omissions.', 'Legal document', 'complianz-terms-conditions' ), '[minimum_age]'),
+		'content'   => sprintf(_x( 'If you are %s years old, your Parent or legal guardian must review and agree to these Terms before you use our website any further, and your Parent or legal guardian will be responsible and liable for all of your acts and omissions.', 'Legal document', 'complianz-terms-conditions' ), '[minimum_age]'),
 		'condition' => array(
 			'minimum_age' => "17",
 			'minimum_age' => "17",
